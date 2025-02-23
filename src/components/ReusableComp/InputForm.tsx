ca440c51032018164337a77ec1f3acad696bd958
AmitBokobza <amitbokobza123@gmail.com>
2025-02-23 20:06:49 +0200

    colors

--- src/components/ReusableComp/InputForm.tsx
+++ src/components/ReusableComp/InputForm.tsx
@@ -22,7 +22,7 @@ const InputForm: FunctionComponent<InputFormProps> = ({
   <div className="relative w-full group">
     <label
       htmlFor={id}
-      className="block text-base font-medium dark:text-gray-100 dark:text-gray-100 mb-1"
+      className="block text-base font-medium light:text-gray-800  mb-1"
     >
       {name.charAt(0).toUpperCase() + name.slice(1)}
     </label>
@@ -32,7 +32,7 @@ const InputForm: FunctionComponent<InputFormProps> = ({
       id={id}
       required={required}
       placeholder=""
-      className="block w-full py-3 px-4 text-base sm:text-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100
+      className="block w-full py-3 px-4 text-base sm:text-lg bg-white light:bg-gray-700 text-gray-900 light:text-gray-100
                  border border-gray-300 dark:border-gray-500 rounded-lg shadow-sm
                  focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500
                  dark:focus:ring-blue-400 dark:focus:border-blue-400 transition-all duration-300"
